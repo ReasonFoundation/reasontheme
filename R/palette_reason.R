fd98c4d0266f5ad9ef6129e6579eccfb563ce7e5
anabbott <anabbott@smu.edu>
2019-07-02 16:18:45 -0500

    palette added

---
+++ R/palette_reason.R
@@ -0,0 +1,25 @@
+#' The Reason Foundation [ggplot2] theme
+#'
+#' Data used by the palettes in the reasontheme package.
+#'
+#' @format A \code{list}.
+#' @export
+palette_reason <- {
+
+  palette <- list(
+    categorical = list(
+      c("#FF6633"),
+      c("#FF6633", "#333333"),
+      c("#FF6633", "#333333", "#6699CC"),
+      c("#FF6633", "#333333", "#6699CC", "#FFCC33"),
+      c("#FF6633", "#333333", "#6699CC", "#FFCC33", "#3399CC"),
+      c("#FF6633", "#333333", "#6699CC", "#FFCC33", "#3399CC", "#3366CC")
+    ),
+
+    pos_neg = list(
+      c("#669933", "#990000")
+    )
+  )
+
+  palette
+}
